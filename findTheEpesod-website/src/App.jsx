65eaee5f38aa251b655e931e0bf082522aa070de
ShacharDoron123 <shachar08.doron@gmail.com>
2025-09-23 10:56:04 +0300

    Save before moving to main

--- findTheEpesod-website/src/App.jsx
+++ findTheEpesod-website/src/App.jsx
@@ -1,17 +1,17 @@
-import { HashRouter as Router, Route, Routes } from 'react-router';
-import HomePage from './src/pages/HomePage.jsx'
-import { Layout } from './src/Layout.jsx';
-function App() {
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+import Layout from './Layout.jsx';
 
+function App() {
   return (
     <Router>
       <Routes>
-        <Route element={<Layout/>}>
-          <Route path="/" element={<HomePage/>}/>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
         </Route>
       </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
--- findTheEpesod-website/src/Layout.jsx
+++ findTheEpesod-website/src/Layout.jsx
@@ -1,17 +1,17 @@
-import { Navbar } from "./src/components/Navbar";
-import { Outlet } from "react-router";
-import Footer from "./src/components/Footer";
-import Header from "./src/components/Header";
+import Navbar from "./components/Navbar.jsx";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer.jsx";
+import Header from "./components/Header.jsx";
 
-export default function Layout(){
-    return(
+export default function Layout() {
+    return (
         <>
-          <Navbar/>
-          <Header/>
-        <main>
-            <Outlet/>
-        </main>
-            <Footer/>
+            <Navbar />
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
         </>
     );
 }
