fb0f6e79e9cd3c10b416f3d43ee9ea2337f1403b
ShacharDoron123 <shachar08.doron@gmail.com>
2025-09-25 16:11:01 +0300

    Improved css and design

--- findTheEpesod-website/src/components/Navbar.jsx
+++ findTheEpesod-website/src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-
   return (
-    <div>
-      <Link to="/">
-        <button>HomePage</button>
-      </Link>
-      <hr />
-    </div>
+    <nav className="navbar">
+      <Link to="/" className="nav-link">Home</Link>
+      <Link to="/movies" className="nav-link">Movies</Link>
+      <Link to="/series" className="nav-link">Series</Link>
+      <Link to="/about" className="nav-link">About</Link>
+    </nav>
   );
 }
