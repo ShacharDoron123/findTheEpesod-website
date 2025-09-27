d6dec182fa0217db984ff544562924f29a4175f4
ShacharDoron123 <shachar08.doron@gmail.com>
2025-09-27 14:46:30 +0300

    css Series card

--- findTheEpesod-website/src/components/Card.jsx
+++ findTheEpesod-website/src/components/Card.jsx
@@ -1,7 +1,7 @@
-export default function Card({ title, description, imageUrl }) {
+export default function Card({ title, description, image }) {
     return (
         <div className="card">
-            <img src={imageUrl} alt={title} className="card-image" />
+            <img src={image} alt={title} className="card-image" />
             <div className="card-content">
                 <h3 className="card-title">{title}</h3>
                 <p className="card-description">{description}</p>
--- findTheEpesod-website/src/pages/Series.jsx
+++ findTheEpesod-website/src/pages/Series.jsx
@@ -1,20 +1,56 @@
-import Table from "../components/Table";   
-import { seriesData } from "../tables-Information/The-Big-Bang-Theory.js";
+import Card from "../components/Card.jsx";
+import { Link } from "react-router-dom";
+import The_Big_Bang_Theory from "../assets/The_Big_Bang_Theory.webp";
 
 export default function Series() {
+  const seriesList = [
+  {
+    title: "The Big Bang Theory",
+    description:
+      "A popular sitcom about a group of friends who are scientists and their interactions with each other and the people around them.",
+    image: The_Big_Bang_Theory,
+  },
+  {
+    title: "Friends",
+    description:
+      "Six friends living in Manhattan navigate life, love, and laughter together.",
+    image: "",
+  },
+  {
+    title: "Brooklyn Nine-Nine",
+    description:
+      "A comedic look at the lives of detectives in a Brooklyn precinct.",
+    image: "",
+  },
+];
+
   return (
     <div>
       <h2>Series Page</h2>
       <p>This is the Series page.</p>
 
-      {Object.entries(seriesData).map(([seasonKey, season]) => (
-        <Table
-          key={seasonKey}
-          season={seasonKey.replace("season", "")}
-          columns={season.columns}
-          data={season.data}
+      <div>
+  <div>
+    <h2>Popular:</h2>
+  </div>
+  <div className="cards-grid">   {/* ⬅️ עטיפה עם className */}
+    {seriesList.map((seriesItem, index) => (
+      <Link
+        key={index}
+        to="/SeriesInfo"
+        state={seriesItem}
+        className="link-wrapper"
+      >
+        <Card
+          title={seriesItem.title}
+          image={seriesItem.image}
+          description={seriesItem.description}
         />
-      ))}
-    </div>
+      </Link>
+    ))}
+  </div>
+ </div>
+</div>
   );
 }
+
